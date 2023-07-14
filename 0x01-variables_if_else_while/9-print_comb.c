9f91da60948fcf71373f3a4dabe8def2e1841ad5
stesoda <sophiadaniel92@gmail.com>
2023-07-14 11:42:39 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,15 @@ int main(void)
 {
 	int x;
 
-	for (x = 0 ; x <= 9 ; x++)
+	for (x = 0 ; x <= 10 ; x++)
 	{
-		if (x == 9)
-			putchar(x + '0');
-		else
+		putchar(x + '0');
+		if (x != 9)
 		{
-			putchar(x + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
