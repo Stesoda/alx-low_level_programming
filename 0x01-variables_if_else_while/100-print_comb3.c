e454e57ed84009b853dbc619947a1208fa00738f
stesoda <sophiadaniel92@gmail.com>
2023-07-14 13:22:42 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,9 +6,25 @@
  */
 int main(void)
 {
-	int x;
+	int x = '0';
+	int y = '0';
 
-	for ()
-		putchar();
-	retun (0);
+	for (y = '0'; y <= '9'; y++)
+	{
+		for (x = '0'; x <= '9'; x++)
+		{
+			if (!((x == y) || (y > x)))
+			{
+				putchar(y);
+				putchar(x);
+				if (!(x == '9' && y == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
